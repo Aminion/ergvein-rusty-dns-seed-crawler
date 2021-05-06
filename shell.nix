39ba90942f2619432a060e023f293c86c3a2a566
Aminion <aminion@protonmail.com>
2021-05-06 13:40:10 +0300

    Add base structure and configs.

---
+++ shell.nix
@@ -0,0 +1,21 @@
+let
+sources = import ./nix/sources.nix;
+pkgs = import sources.nixpkgs {};
+in with pkgs;
+let merged-openssl = symlinkJoin { name = "merged-openssl"; paths = [ openssl.out openssl.dev ]; };
+in stdenv.mkDerivation rec {
+  name = "rust-env";
+  env = buildEnv { name = name; paths = buildInputs; };
+
+  buildInputs = [
+    rustup
+    clang
+    llvmPackages_11.libclang
+    openssl
+    llvm_11
+  ];
+  shellHook = ''
+  export LIBCLANG_PATH="${llvmPackages.libclang}/lib"
+  export OPENSSL_DIR="${merged-openssl}"
+  '';
+}
---
+++ src/main.rs
@@ -0,0 +1,18 @@
+extern crate byteorder;
+extern crate bytes;
+extern crate clap;
+extern crate fs2;
+extern crate reqwest;
+extern crate serde;
+extern crate tokio;
+extern crate tokio_stream;
+extern crate tokio_util;
+extern crate lazy_static;
+extern crate warp;
+
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
